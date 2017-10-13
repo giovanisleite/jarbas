f0c240b849feff37c2d2b6d1d3a8cd4ab7334d35
Giovani <sousa_giovani@hotmail.com>
2017-10-13 01:25:26 -0300

    add chamber_of_deputies description

---
+++ jarbas/chamber_of_deputies/app.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ChamberOfDeputiesConfig(AppConfig):
+    name = 'jarbas.chamber_of_deputies'
+    verbose_name = 'Cota para Exercício da Atividade Parlamentar - Câmara dos Deputados'
